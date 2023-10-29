a6a5d2faea1452a8de98e6da73546d2435e6a1b7
Saryrndeth <Saryrndeth@gmail.com>
2023-10-30 02:19:01 +0900

    틀린 단어 출력

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from wordlist import worddict
 from random import choice
+import os
+import time
 
 
 wrong_count: int = 0
+wrong = []
 correct = []
 while len(correct) != 50:
     key = choice(list(worddict.keys()))
@@ -15,7 +18,12 @@ while len(correct) != 50:
         correct.append(key)
     else:
         wrong_count += 1
+        if not(key in wrong):
+            wrong.append(key)
         print(f'오답, 정답은: {worddict[key]}')
-
+        time.sleep(2)
 
 print(f'종료, 틀린 횟수: {wrong_count}')
+print(f'한번 이상 틀린 단어: ')
+for key in wrong:
+    print(f'    {key}: {worddict[key]}')
